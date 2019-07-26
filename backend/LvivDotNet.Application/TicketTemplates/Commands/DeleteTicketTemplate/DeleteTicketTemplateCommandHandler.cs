2ad2ef0f7aa207bd95891850db0562b1a844abc4
Andwer <caballiero777@gmail.com>
2019-07-26 14:13:18 +0300

    Fix last stylecop issue connected to null checks for delete ticket template command.

--- backend/LvivDotNet.Application/TicketTemplates/Commands/DeleteTicketTemplate/DeleteTicketTemplateCommandHandler.cs
+++ backend/LvivDotNet.Application/TicketTemplates/Commands/DeleteTicketTemplate/DeleteTicketTemplateCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
@@ -31,6 +32,7 @@ namespace LvivDotNet.Application.TicketTemplates.Commands.DeleteTicketTemplate
         }
 
         /// <inheritdoc/>
+        [SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "We already have a not-null check for request in MediatR")]
         protected override async Task<Unit> Handle(DeleteTicketTemplateCommand request, IDbConnection connection, IDbTransaction transaction, CancellationToken cancellationToken)
         {
             var deletedCount = await connection.ExecuteAsync(DeleteTicketTemplateSqlCommand, request, transaction)
